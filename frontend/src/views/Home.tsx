4e381af82057c656c0cdf46c5945114404121b00
Bipin <beepeenstha10@gmail.com>
2026-02-18 15:18:28 +0545

    feat: add reference and remove hero section

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,5 +1,6 @@
 import FileUpload from "../components/FileUpload";
-import Hero from "../components/Hero";
+// import Hero from "../components/Hero";
+import { useRef } from "react";
 
 const Home = () => {
   const weekday = [
@@ -11,14 +12,21 @@ const Home = () => {
     "Friday",
     "Saturday",
   ];
+
+  const fileUploadRef = useRef<HTMLDivElement>(null);
+
+  // const scrollToSection = () => {
+  //   fileUploadRef.current?.scrollIntoView({ behavior: "smooth" });
+  // };
+
   return (
     <div>
-      <Hero />
+      {/* <Hero onButtonClick={scrollToSection} /> */}
       <div className="flex-1 flex flex-col gap-6 items-center justify-center">
         <h1 className="text-3xl md:text-5xl">
           Happy {weekday[new Date().getDay()]}
         </h1>
-        <FileUpload />
+        <FileUpload ref={fileUploadRef} />
       </div>
     </div>
   );
