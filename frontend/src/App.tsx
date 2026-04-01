81b151cda2cd3b3d5460b74b631d424cfe59a82f
Bipin <beepeenstha10@gmail.com>
2026-04-01 23:31:08 +0545

    feat: add logic for the display of navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
-import { Outlet } from "react-router";
-import Footer from "./components/layout/Footer";
+import { Outlet, useLocation } from "react-router";
+// import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 
 const App = () => {
+  const location = useLocation();
+
+  // defining routes where navbar should be hidden
+  const hideNavbarRoutes = ["/dashboard", "/history"];
+
+  const showNavbar = !hideNavbarRoutes.includes(location.pathname);
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="sticky top-0 left-0 z-50">
-        <Navbar />
-      </div>
+    <div className="flex flex-col min-h-screen overflow-x-hidden">
+      {showNavbar && (
+        <div className="sticky top-0 left-0 z-50">
+          <Navbar />
+        </div>
+      )}
       <main className="flex-1 flex flex-col">
         <Outlet />
       </main>
-      <div>
+      {/* <div>
         <Footer />
-      </div>
+      </div> */}
     </div>
   );
 };
