a829212346b3db07650693ffb2f2bdb031ca454a
Bipin <beepeenstha10@gmail.com>
2026-04-02 23:12:39 +0545

    change navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet, useLocation } from "react-router";
 // import Footer from "./components/layout/Footer";
-import Navbar from "./components/layout/Navbar";
+import { MainNavbar } from "./components/layout/Navbar";
 
 const App = () => {
   const location = useLocation();
 
   // defining routes where navbar should be hidden
-  const hideNavbarRoutes = ["/dashboard", "/history"];
+  const hideNavbarRoutes = ["/dashboard", "/history", "/login", "/signup"];
 
   const showNavbar = !hideNavbarRoutes.includes(location.pathname);
 
@@ -14,7 +14,7 @@ const App = () => {
     <div className="flex flex-col min-h-screen overflow-x-hidden">
       {showNavbar && (
         <div className="sticky top-0 left-0 z-50">
-          <Navbar />
+          <MainNavbar />
         </div>
       )}
       <main className="flex-1 flex flex-col">
