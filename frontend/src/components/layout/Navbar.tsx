262db76a777afc0a0d184a5c0aaeae238751c770
Bipin <beepeenstha10@gmail.com>
2026-04-02 21:14:59 +0545

    feat: add links to different section and pages

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,17 +1,51 @@
 import { Link } from "react-router";
+import Button from "../common/Button";
 
 const Navbar = () => {
   return (
-    <div className="flex py-5 px-4 md:px-6 bg-white/30 backdrop-blur-md">
-      <div className="w-30 md:w-30 cursor-pointer border border-gray-300 rounded-5xl">
-        <Link to="/">
-          <img
-            src="https://res.cloudinary.com/dx0rhbj0a/image/upload/v1766513552/b568c48c-996d-42ae-a87e-9e3f725de7ce_qmzjak.png"
-            alt="DOCCAT+"
-          />
+    <header className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-8 py-5 bg-white border-b border-[#9e9e9e8f]">
+      <div className="flex items-center gap-8">
+        <Link
+          to="/"
+          className="text-2xl font-semibold text-onBackground hover:opacity-80 transition-opacity"
+          aria-label="DocCat+ Home"
+        >
+          DocCat+
+        </Link>
+        <nav className="hidden gap-6 text-sm text-secondary md:flex">
+          <a
+            href="#features"
+            className="text-onBackground hover:text-primary transition-colors cursor-pointer"
+          >
+            Features
+          </a>
+          <a
+            href="#about"
+            className="text-onBackground hover:text-primary transition-colors cursor-pointer"
+          >
+            About
+          </a>
+          <a
+            href="#contact"
+            className="text-onBackground hover:text-primary transition-colors cursor-pointer"
+          >
+            Contact
+          </a>
+        </nav>
+      </div>
+      <div className="flex items-center gap-3 text-sm">
+        <Link to="/grid">Grid</Link>
+        <Link
+          to="/login"
+          className="text-onBackground hover:text-primary transition-colors px-4 py-2 rounded-base"
+        >
+          Log in
+        </Link>
+        <Link to="/signup">
+          <Button>Get Started</Button>
         </Link>
       </div>
-    </div>
+    </header>
   );
 };
 
