30527de0b0615dbd3fcaa97d4e44ccb5070d3c80
Bipin <beepeenstha10@gmail.com>
2026-05-02 09:43:28 +0545

    feat: add protected route

---
+++ frontend/src/components/common/ProtectedRoute.tsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router";
+import type { RootState } from "../../store/store";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+  redirectTo?: string;
+}
+
+const ProtectedRoute = ({
+  children,
+  redirectTo = "/login",
+}: ProtectedRouteProps) => {
+  const isAuthenticated = useSelector(
+    (state: RootState) => !!state.auth.access_token,
+  );
+
+  if (!isAuthenticated) {
+    return <Navigate to={redirectTo} replace />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
