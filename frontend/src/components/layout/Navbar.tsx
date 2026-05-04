a3d6f000a45a785c2ff23db7bb32df30646c0cd2
Bipin <beepeenstha10@gmail.com>
2026-05-04 14:14:34 +0545

    feat: remove the logout logic and links

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,21 +1,7 @@
-import { Link, useNavigate } from "react-router";
-import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router";
 import Button from "../common/Button";
-import { clearAuth } from "../../store/authSlice";
-import type { RootState } from "../../store/store";
 
 export const MainNavbar = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const accessToken = useSelector(
-    (state: RootState) => state.auth.access_token,
-  );
-
-  const handleLogout = () => {
-    dispatch(clearAuth());
-    navigate("/", { replace: true });
-  };
-
   return (
     <header className="fixed top-0 left-0 right-0 z-50  bg-white border-b border-[#9e9e9e8f]">
       <div className="max-w-7xl mx-auto flex items-center justify-between py-5">
@@ -49,36 +35,16 @@ export const MainNavbar = () => {
           </nav>
         </div>
         <div className="flex items-center gap-3 text-sm">
-          <Link to="/grid">Grid</Link>
-          {accessToken ? (
-            <>
-              <Link
-                to="/dashboard"
-                className="text-onBackground hover:text-primary transition-colors px-4 py-2 rounded-base"
-              >
-                Dashboard
-              </Link>
-              <button
-                type="button"
-                onClick={handleLogout}
-                className="text-onBackground hover:text-primary transition-colors px-4 py-2 rounded-base cursor-pointer"
-              >
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link
-                to="/login"
-                className="text-onBackground hover:text-primary transition-colors px-4 py-2 rounded-base"
-              >
-                Log in
-              </Link>
-              <Link to="/signup">
-                <Button>Get Started</Button>
-              </Link>
-            </>
-          )}
+          {/* <Link to="/grid">Grid</Link> */}
+          <Link
+            to="/login"
+            className="text-onBackground hover:text-primary transition-colors px-4 py-2 rounded hover:bg-[#E8E8E8]"
+          >
+            Log in
+          </Link>
+          <Link to="/signup">
+            <Button>Get Started</Button>
+          </Link>
         </div>
       </div>
     </header>
